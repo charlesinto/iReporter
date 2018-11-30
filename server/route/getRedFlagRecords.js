47170d43063c8983573b73ad15486a4dde185ada
charles <charles.onuorah@yahoo.com>
2018-11-30 15:57:22 +0100

    delete a red flag record/162325252

--- server/route/getRedFlagRecords.js
+++ server/route/getRedFlagRecords.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import { getRedFlags, getARecord, postRecord, updateLocation, updateComment } from '../controller';
+import { getRedFlags, getARecord, postRecord, updateLocation, updateComment, deleteRecord } from '../controller';
 const router = express.Router();
 
 router.get('/', getRedFlags);
@@ -8,6 +8,6 @@ router.get('/:id', getARecord);
 router.post('/', postRecord);
 router.patch('/:id/location', updateLocation);
 router.patch('/:id/comment', updateComment);
-
+router.delete('/:id', deleteRecord);
 
 export default router;
