233adb982029c0980b007aed33539fc072cf1638
charles <charles.onuorah@yahoo.com>
2018-11-30 01:14:37 +0100

    user should be able to get a specific record/ 162324394

--- server/route/getRedFlagRecords.js
+++ server/route/getRedFlagRecords.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import path from 'path';
-import { getRedFlags } from '../controller';
+import { getRedFlags, getARecord } from '../controller';
 const router = express.Router();
 
 router.get('/', getRedFlags);
+router.get('/:id', getARecord);
 
 
 export default router;
