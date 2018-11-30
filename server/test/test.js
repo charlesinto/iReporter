f2990648a6c2fd751622daab1d0bd585cd709891
charles <charles.onuorah@yahoo.com>
2018-11-30 15:04:22 +0100

    update a record geolocation/1623244566

--- server/test/test.js
+++ server/test/test.js
@@ -18,6 +18,10 @@ const newRecord = {
     "comment":"london bridge is falling"
 }
 
+const location = {
+    location: '6.4444,3.20000'
+}
+
 describe('It should test all the end points', () => {
     describe('it should get all red flag records',() => {
         it('response should be an object', function(done){
@@ -116,6 +120,41 @@ describe('It should test all the end points', () => {
        
         it('data should be an array', function(done){
             chai.request(app).post('/api/v1/red-flags').type('form').set('content-type', 'application/json').send(newRecord).end(function(err,res){
+                expect(res.body.data).to.be.an('array');
+                done();
+            })
+        })
+        
+    })
+    describe('it should patch a red flag records',() => {
+        it('response should be an object', function(done){
+            chai.request(app).patch('/api/v1/red-flags/13/location').type('form').set('content-type', 'application/json').send(location).end(function(err,res){
+                expect(res).to.be.an('object');
+                done();
+            })
+        })
+        it('response to have property status', function(done){
+            chai.request(app).patch('/api/v1/red-flags/13/location').type('form').set('content-type', 'application/json').send(location).end(function(err,res){
+                expect(res.body).to.have.property('status');
+                done();
+            })
+        })
+        it('response to have property data', function(done){
+            chai.request(app).patch('/api/v1/red-flags/13/location').type('form').set('content-type', 'application/json').send(location).end(function(err,res){
+                expect(res.body).to.have.property('data');
+                done();
+            })
+        })
+        it('response should have a status of 201',(done)=>{
+            chai.request(app).patch('/api/v1/red-flags/13/location').type('form').set('content-type', 'application/json').send(location).end(function(err,res){
+                
+                expect(res).to.have.status(202);
+                done();
+            })
+        })
+       
+        it('data should be an array', function(done){
+            chai.request(app).patch('/api/v1/red-flags/13/location').type('form').set('content-type', 'application/json').send(location).end(function(err,res){
                 expect(res.body.data).to.be.an('array');
                 done();
             })
