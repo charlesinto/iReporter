a008f5271ca938640755ed075b6e67d2cc9323ff
charles <charles.onuorah@yahoo.com>
2018-12-11 20:39:44 +0100

    feat(endpoint): Get an Intervention Record Implement JWT authentication Verify Token Get a record createdby the user

--- server/route/interventionRecord.js
+++ server/route/interventionRecord.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import path from 'path';
 import { VerifyToken } from '../middleware'
-import { getInterventionRecord } from '../controller';
+import { getInterventionRecord, getASingleRecord } from '../controller';
 const router = express.Router();
 
 router.get('/',VerifyToken, getInterventionRecord);
+router.get('/:id', VerifyToken, getASingleRecord);
 
 export default router; 
