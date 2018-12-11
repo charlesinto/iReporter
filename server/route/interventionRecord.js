ec46fea535a4dadb5a6b819c7dfc7577aa83bcc3
charles <charles.onuorah@yahoo.com>
2018-12-11 22:20:40 +0100

    feat(endpoint): Update intervention record location

--- server/route/interventionRecord.js
+++ server/route/interventionRecord.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import path from 'path';
 import { VerifyToken } from '../middleware'
-import { getInterventionRecord, getASingleRecord, createNewRecord, updateInterventionComment } from '../controller';
+import { 
+    getInterventionRecord, getASingleRecord, createNewRecord, updateInterventionComment, updateInterventionLocation
+
+} from '../controller';
 const router = express.Router();
 
 router.get('/',VerifyToken, getInterventionRecord);
 router.get('/:id', VerifyToken, getASingleRecord);
 router.post('/', VerifyToken, createNewRecord);
-router.patch('/:id/comment', VerifyToken, updateInterventionComment)
+router.patch('/:id/comment', VerifyToken, updateInterventionComment);
+router.patch('/:id/location', VerifyToken, updateInterventionLocation);
+
 export default router; 
