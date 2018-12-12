922100e73f12c1a4f94f2d8e1de7c19a725ed3f9
charles <charles.onuorah@yahoo.com>
2018-12-12 10:52:57 +0100

    ch(UI): Remove red border on button Add background Image to login page Add footer to login page Redesign the login form Capitalize links in the user page

--- UI/dependencies/js/login.js
+++ UI/dependencies/js/login.js
@@ -10,16 +10,22 @@ const setUpEvents = () => {
     document.getElementsByClassName('signup')[0].style.background = "none";
     document.getElementsByClassName('signup')[0].addEventListener('click', (e) => {
         e.stopPropagation();
+        e.target.classList.toggle('active');
         document.getElementsByClassName('login-form')[0].style.display = 'none';
-        document.getElementsByClassName('login')[0].style.background = "none";
+       // document.getElementsByClassName('login')[0].style.background = "none";
+    //    document.getElementsByClassName('login')[0].classList.toggle('active');
+       document.getElementsByClassName('login')[0].classList.toggle('active');
         e.target.style.background = '#fff';
         document.getElementsByClassName('signup-form')[0].style.display="block";
         
     })
     document.getElementsByClassName('login')[0].addEventListener('click', (e) => {
         e.stopPropagation();
+        e.target.classList.toggle('active');
         document.getElementsByClassName('signup-form')[0].style.display = 'none';
-        document.getElementsByClassName('signup')[0].style.background = "none";
+        // document.getElementsByClassName('signup')[0].style.background = "none";
+        // document.getElementsByClassName('login')[0].classList.toggle('active');
+        document.getElementsByClassName('signup')[0].classList.toggle('active');
         e.target.style.background = '#fff';
         document.getElementsByClassName('login-form')[0].style.display="block";
     })
